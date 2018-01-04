bc6b4d0569870994fff1fa08321e434b61882bc6
Electronic Armory <mike@electronicarmory.com>
2018-01-03 17:04:13 -0700

    Adding Xcode project and source files for Location and Geofencing tutorials.

---
+++ Location/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Electronic Armory on 12/15/17.
+//  Copyright © 2017 Electronic Armory. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+        locationManager.distanceFilter = 100
+        
+        let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(43.61871, -116.214607), radius: 100, identifier: "Boise")
+        
+        locationManager.startMonitoring(for: geoFenceRegion)
+        
+//        locationManager.stopUpdatingLocation()
+    }
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for currentLocation in locations{
+            print("\(index): \(currentLocation)")
+            // "0: [locations]"
+        }
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered: \(region.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited: \(region.identifier)")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
